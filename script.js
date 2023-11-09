3190a941f661c18282ed9edfc7c3a06e9414a5b9
blackclover221 <yasminismail33@gmail.com>
2023-11-09 21:12:56 +0000

    changed the script.js and html to fix the error within the API so now it can make calls and give the results of a 5 day weather API

--- script.js
+++ script.js
@@ -1,68 +1,67 @@
-// Replace with your OpenWeatherMap API key
-const apiKey = '4f04a5c312a48472678fe0c9cca5b26e';
-const apiUrl = 'https://api.openweathermap.org/data/2.5';
+document.addEventListener('DOMContentLoaded', () => {
+    const apiKey = '4f04a5c312a48472678fe0c9cca5b26e';
+    const currentWeatherUrl = 'https://api.openweathermap.org/data/2.5/weather';
+    const forecastUrl = 'https://api.openweathermap.org/data/2.5/forecast';
 
-// Elements
-const cityInput = document.getElementById('city-input');
-const searchForm = document.getElementById('search-form');
-const weatherInfo = document.getElementById('weather-info');
-const searchHistory = document.getElementById('search-history');
+    const cityInput = document.getElementById('city-input');
+    const searchForm = document.getElementById('search-form');
+    const weatherInfo = document.getElementById('weather-info');
 
+    searchForm.addEventListener('submit', async (e) => {
+        e.preventDefault();
+        const city = cityInput.value.trim();
 
-searchForm.addEventListener('submit', async (e) => {
-    e.preventDefault();
-    const city = cityInput.value;
-    if (city.trim() === '') return;
+        if (!city) {
+            alert('Please enter a city name.');
+            return;
+        }
 
+        try {
+            const currentWeatherResponse = await fetch(`${currentWeatherUrl}?q=${city}&appid=${apiKey}`);
+            const forecastResponse = await fetch(`${forecastUrl}?q=${city}&appid=${apiKey}`);
 
-    const forecastData = await get5DayForecast(city);
-    if (forecastData) {
-        displayCity5DayForecast(city, forecastData);
-        cityInput.value = '';
-        addCityToHistory(city);
+            const currentWeatherData = await currentWeatherResponse.json();
+            const forecastData = await forecastResponse.json();
+
+            if (currentWeatherResponse.ok && forecastResponse.ok) {
+                displayCurrentWeather(currentWeatherData);
+                display5DayForecast(forecastData);
+            } else {
+                weatherInfo.innerHTML = `City not found. Please try again.`;
+            }
+        } catch (error) {
+            console.error('Error fetching weather data:', error);
+            weatherInfo.innerHTML = 'Error fetching weather data. Please try again later.';
+        }
+    });
+
+    function displayCurrentWeather(data) {
+        const temperature = (data.main.temp - 273.15).toFixed(2);
+        const weatherDescription = data.weather[0].description;
+
+        const weatherHtml = `
+            <h2>Current Weather in ${data.name}</h2>
+            <p>Temperature: ${temperature}°C</p>
+            <p>Description: ${weatherDescription}</p>
+        `;
+
+        weatherInfo.innerHTML = weatherHtml;
+    }
+
+    function display5DayForecast(data) {
+        const forecastHtml = `
+            <h2>5-Day Forecast</h2>
+            <div class="forecast-container">
+                ${data.list.slice(0, 5).map(day => `
+                    <div class="forecast-day">
+                        <p>Date: ${day.dt_txt}</p>
+                        <p>Temperature: ${(day.main.temp - 273.15).toFixed(2)}°C</p>
+                        <p>Description: ${day.weather[0].description}</p>
+                    </div>
+                `).join('')}
+            </div>
+        `;
+
+        weatherInfo.innerHTML += forecastHtml;
     }
 });
-
-async function get5DayForecast(city) {
-    try {
-        const response = await fetch(`${apiUrl}/forecast?q=${city}&appid=${apiKey}`);
-        const data = await response.json();
-        return data;
-    } catch (error) {
-        return null;
-    }
-}
-
-
-function displayCity5DayForecast(city, forecastData) {
-    if (forecastData === null) {
-        weatherInfo.innerHTML = 'City not found.';
-        return;
-    }
-
-    const forecastHtml = `
-    <h2>5-Day Forecast for ${city}</h2>
-    <div class="row">
-        ${forecastData.list.slice(0, 5).map(day => `
-            <div class="col-lg-2">
-                <div class="card mb-3">
-                    <div class="card-body">
-                        <h5 class="card-title">${day.weather[0].description}</h5>
-                        <p class="card-text">Max Temp: ${(day.main.temp_max - 273.15).toFixed(2)}°C</p>
-                        <p class="card-text">Min Temp: ${(day.main.temp_min - 273.15).toFixed(2)}°C</p>
-                    </div>
-                </div>
-            </div>
-        `).join('')}
-    </div>
-  `;
-
-    weatherInfo.innerHTML = forecastHtml;
-}
-
-function addCityToHistory(city) {
-    const listItem = document.createElement('li');
-    listItem.textContent = city;
-    listItem.classList.add('list-group-item');
-    searchHistory.appendChild(listItem);
-}
